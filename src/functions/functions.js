3e80ee77476ea467495a1df715209cd0441ad8bd
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2023-11-02 12:25:47 +0000

    First add-in function that fecthes github info through github api based on the nickname provided. With a custom namespace.

---
+++ src/functions/functions.js
@@ -0,0 +1,90 @@
+﻿/* global clearInterval, console, setInterval */
+
+/**
+ * Add two numbers
+ * @customfunction
+ * @param {number} first First number
+ * @param {number} second Second number
+ * @returns {number} The sum of the two numbers.
+ */
+export function add(first, second) {
+  return first + second;
+}
+
+/**
+ * Displays the current time once a second
+ * @customfunction
+ * @param {CustomFunctions.StreamingInvocation<string>} invocation Custom function invocation
+ */
+export function clock(invocation) {
+  const timer = setInterval(() => {
+    const time = currentTime();
+    invocation.setResult(time);
+  }, 1000);
+
+  invocation.onCanceled = () => {
+    clearInterval(timer);
+  };
+}
+
+/**
+ * Returns the current time
+ * @returns {string} String with the current time formatted for the current locale.
+ */
+export function currentTime() {
+  return new Date().toLocaleTimeString();
+}
+
+/**
+ * Increments a value once a second.
+ * @customfunction
+ * @param {number} incrementBy Amount to increment
+ * @param {CustomFunctions.StreamingInvocation<number>} invocation
+ */
+export function increment(incrementBy, invocation) {
+  let result = 0;
+  const timer = setInterval(() => {
+    result += incrementBy;
+    invocation.setResult(result);
+  }, 1000);
+
+  invocation.onCanceled = () => {
+    clearInterval(timer);
+  };
+}
+
+/**
+ * Writes a message to console.log().
+ * @customfunction LOG
+ * @param {string} message String to write.
+ * @returns String to write.
+ */
+export function logMessage(message) {
+  console.log(message);
+
+  return message;
+}
+
+/**
+ * Returns Github user info
+ * @customfunction
+ * @param {string} nickname Nickname of github user
+ * @returns Info of user in github
+ */
+
+export async function getGithubInfo(nickname) {
+  try {
+    const url = `https://api.github.com/users/${nickname}`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const json = await response.json();
+    return `${json["name"]} - ${json["bio"]}`;
+  }
+  catch (error) {
+    return error;
+  }
+}
