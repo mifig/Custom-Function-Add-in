78839634f6bc842f3b5d1f32f5861b50241b1058
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2023-11-02 17:07:45 +0000

    Changed manifest.xml file to change logo and name of ribbon. Need to search how to create custom tab

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,94 +1,4 @@
-/* global fetch, clearInterval, console, setInterval */
-
-/**
- * Add two numbers
- * @customfunction
- * @param {number} first First number
- * @param {number} second Second number
- * @returns {number} The sum of the two numbers.
- */
-export function add(first, second) {
-  return first + second;
-}
-
-/**
- * Displays the current time once a second
- * @customfunction
- * @param {CustomFunctions.StreamingInvocation<string>} invocation Custom function invocation
- */
-export function clock(invocation) {
-  const timer = setInterval(() => {
-    const time = currentTime();
-    invocation.setResult(time);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
-
-/**
- * Returns the current time
- * @returns {string} String with the current time formatted for the current locale.
- */
-export function currentTime() {
-  return new Date().toLocaleTimeString();
-}
-
-/**
- * Increments a value once a second.
- * @customfunction
- * @param {number} incrementBy Amount to increment
- * @param {CustomFunctions.StreamingInvocation<number>} invocation
- */
-export function increment(incrementBy, invocation) {
-  let result = 0;
-  const timer = setInterval(() => {
-    result += incrementBy;
-    invocation.setResult(result);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
-
-/**
- * Writes a message to console.log().
- * @customfunction LOG
- * @param {string} message String to write.
- * @returns String to write.
- */
-export function logMessage(message) {
-  console.log(message);
-
-  return message;
-}
-
-/**
- * Returns Github user info
- * @customfunction
- * @param {string} nickname Nickname of github user
- * @returns Info of user in github
- */
-
-export async function getGithubInfo(nickname) {
-  try {
-    const url = `https://api.github.com/users/${nickname}`;
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-
-    const json = await response.json();
-    return `${json["name"]} - ${json["bio"]}`;
-  }
-  catch (error) {
-    return error;
-  }
-}
-
+/* global */
 
 /**
  * Returns churn rate of client
@@ -99,8 +9,8 @@ export async function getGithubInfo(nickname) {
 
 export async function churn(client_id) {
   const url = `https://protected-badlands-28432-72b308c2a4a7.herokuapp.com/api/v1/clients/${client_id}`;
-  // const url = `https://api.github.com/users/mifig`;
   const response = await fetch(url);
   const json = await response.json();
+
   return json["churn"];
 }
